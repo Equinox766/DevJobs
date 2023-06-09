dfe0a65f33ae7b1d0a9a89d71472827b6dd6447d
Equinox766 <equinox766@gmail.com>
2023-06-09 16:26:08 -0400

    Agregando un policy para proteger el edit

--- app/Http/Controllers/Home/VacanteController.php
+++ app/Http/Controllers/Home/VacanteController.php
@@ -45,6 +45,7 @@ class VacanteController extends Controller
      */
     public function edit(Vacante $vacante)
     {
+        $this->authorize('update', $vacante);
         return view('vacantes.edit', compact('vacante'));
     }
 
